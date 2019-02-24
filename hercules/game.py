84751a5ea09278260979f0a5fb228601947a9ab6
Rohit Sanbhadti <rohits47@gmail.com>
2019-02-23 17:26:20 -0800

    added requirements and stubbed game code

--- hercules/game.py
+++ hercules/game.py
@@ -10,3 +10,15 @@ class Game(object):
 
     def __init__(self):
         pass
+
+    def play(self):
+        """
+        Entrypoint for starting a game. This method should loop until the
+        game is over.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.play()
